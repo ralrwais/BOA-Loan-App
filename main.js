05a3336ac29919d4cb794588d538dbcf64fc8a38
Rima Alrwais <ralrwais@ralrwais-ltm4.internal.salesforce.com>
2018-01-15 15:47:47 -0800

    access token

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 let uploadToBox = () => {
+	console.log('button clicked');
 	var userFile = document.getElementById('userFile');
 	var form = new FormData();
-
 	form.append('file', userFile);
-	form.append('parent_id', '1233456');
+	form.append('parent_id', '0');
 
 	$.ajax({
 		url: 'https://upload.box.com/api/2.0/files/content',
-		headers: {Authorization: 'Bearer XsDVMn19tkuIpV9x7bOj8jQDkgPhuaSr'},
+		headers: {Authorization: 'Bearer HPrFm8V0mawpSycKQDc7FxVyW1grXbqH'},
 		type: 'POST',
-		dataType: 'json',
+		processData: false,
+		contentType: false,
 		data: form
-	}).then((response) => {
-		let fileName = response.entries[0].name;
-		let fileID = response.entries[0].id;
+	}).complete((data) => {
+		console.log(data.response);
 	})
 }
 
