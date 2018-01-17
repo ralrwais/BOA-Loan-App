2aeb73a3f56f0b2fbde770048b9ef11a45f9e329
Rima Alrwais <ralrwais@ralrwais-ltm4.internal.salesforce.com>
2018-01-16 22:17:44 -0800

    activate server

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// const express = require('express'),
-// 	  path = require("path"),
-// 	  bodyParser = require("body-parser"),
-// 	  request = require("request"),
-// 	  BoxSDK = require('box-node-sdk'),
-// 	  fs = require('fs');
+const express = require('express'),
+	  path = require("path"),
+	  bodyParser = require("body-parser"),
+	  request = require("request"),
+	  BoxSDK = require('box-node-sdk'),
+	  fs = require('fs');
 
-// const app = express();
-// var PORT = process.env.PORT || 1226;
+const app = express();
+var PORT = process.env.PORT || 1226;
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// // var sdk = new BoxSDK({  clientID: "4vik07rxtygughusywilpawnvwr48kob",  clientSecret: "8CL9ztMjbGdLDz6uOvgy3ElSj1orPtPK"});
-// // var client = sdk.getBasicClient("65wKOSh06weAuaOfUli3OHHwaXvgufHi");
-
-
+// var sdk = new BoxSDK({  clientID: "4vik07rxtygughusywilpawnvwr48kob",  clientSecret: "8CL9ztMjbGdLDz6uOvgy3ElSj1orPtPK"});
+// var client = sdk.getBasicClient("65wKOSh06weAuaOfUli3OHHwaXvgufHi");
 
 
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "/index.html"));
-// });
 
-// app.listen(PORT, function() {
-//   console.log("App listening on PORT " + PORT);
-// });
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
